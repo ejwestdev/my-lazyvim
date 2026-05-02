d93c612a9400b6f68181e0e7bbe467ed49dce344
Edward West <edwardwestdev@gmail.com>
2026-05-02 17:18:16 -0400

    catpuccin frappe

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      flavour = "frappe",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
